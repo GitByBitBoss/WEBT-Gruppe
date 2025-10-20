697c9923632708946960db6ece7b978fbecc5609
Manuel Stanculovic <2058@htl.rennweg.at>
2025-10-20 16:38:42 +0200

    adding pictures and correcting review disaster

--- src/CarTheoryQuestion.php
+++ src/CarTheoryQuestion.php
@@ -1,6 +1,15 @@
 <?php
 require_once "Question.php";
 class CarTheoryQuestion extends Question {
+    
+
+    function getHTML(): string {
+        return "
+        <h2>{$this->getCategory()}</h2>
+        <h3>{$this->getName()}</h3>
+        <img src='../images/car.svg' width='2%' height='2%' />";
+     }
+
     public function __construct($name,$category) {
         parent::__construct($name,$category);
     }
--- src/MotorcycleTheoryQuestion.php
+++ src/MotorcycleTheoryQuestion.php
@@ -4,4 +4,12 @@ class MotorcycleTheoryQuestion extends Question {
     public function __construct($name,$category) {
         parent::__construct($name,$category);
     }
+
+    
+    function getHTML(): string {
+        return "
+        <h2>{$this->getCategory()}</h2>
+        <h3>{$this->getName()}</h3>
+        <img src='../images/motorcycle.svg' width='2%' height='2%' />";
+     }
 }
