946555c50990e23d871fbbbc8e4527e63c1d8f6d
Manuel Stanculovic <2058@htl.rennweg.at>
2026-01-16 21:56:49 +0100

    Finished US3

--- public/createSession.php
+++ public/createSession.php
@@ -1,13 +1,22 @@
 <?php
+    /*  */
+    session_set_cookie_params([
+        'lifetime' => 0,
+        'path' => '/',
+        'secure' => false,    //https funktioniert nicht weil wir kein ssl haben 
+        'httponly' => true,   
+        'samesite' => 'Lax'   // CSRF-Risiko reduzieren (häufig Lax/Strict)
+    ]);
+    ini_set('session.gc_maxlifetime', 10);
     session_start();
 
 
-    if(isset($_SESSION['userID'])) {
+    if (!isset($_SESSION['userID'])) {
+        
+        $_SESSION['userID'] = random_int(1, 20);
+        echo "Session gestartet :D";
+    } else {
+        session_regenerate_id(true);    
+        var_dump(session_get_cookie_params()) ; 
         echo "deine id ist: " . $_SESSION['userID'];
     }
-
-    else {
-        echo "Session gestartet :D";
-        $_SESSION['userID'] = random_int(1,20);
-    }
-
