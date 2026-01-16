bc85ed8d59285f07c0bb9a099f320fc671f7c20a
Manuel Stanculovic <2058@htl.rennweg.at>
2026-01-16 22:19:36 +0100

    Finished US4

--- public/createSession.php
+++ public/createSession.php
@@ -7,16 +7,26 @@
         'httponly' => true,   
         'samesite' => 'Lax'   // CSRF-Risiko reduzieren (häufig Lax/Strict)
     ]);
-    ini_set('session.gc_maxlifetime', 10);
+    ini_set('session.gc_maxlifetime', 10); 
+    ini_set('session.gc_probability', 1);   
+    ini_set('session.gc_divisor', 1);
     session_start();
 
 
     if (!isset($_SESSION['userID'])) {
-        
         $_SESSION['userID'] = random_int(1, 20);
         echo "Session gestartet :D";
     } else {
         session_regenerate_id(true);    
-        var_dump(session_get_cookie_params()) ; 
         echo "deine id ist: " . $_SESSION['userID'];
     }
+
+    // Zeige die aktuelle Session-ID
+    echo "<br>Aktuelle Session-ID: " . session_id();
+
+    // Zeige die Cookie-Parameter an
+    echo "<br>Session-Cookie-Parameter: ";
+    var_dump(session_get_cookie_params());
+
+    // Manuell die Garbage Collection auslösen
+?>
